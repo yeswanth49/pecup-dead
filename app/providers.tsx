6e0ab64b34bb387e1db9bf8b53008bfa60e0053f
yeswanth49 <sinchan123v@gmail.com>
2025-10-29 14:42:33 +0530

    Set default theme to light instead of dark

--- app/providers.tsx
+++ app/providers.tsx
@@ -12,7 +12,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
       <ProfileProvider>
         <ThemeProvider
           attribute="class"
-          defaultTheme="white"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
